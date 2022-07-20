3e0704192a96a2c1d12d0761106208d87a952318
Burhanuddin <burhanuddindalal22@gmail.com>
2022-07-19 22:13:19 -0400

    Add Entity Class and Certain Attributes

---
+++ src/Entity/Entity.java
@@ -0,0 +1,65 @@
+package Entity;
+
+import Main.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    //Game Panel
+    GamePanel gp;
+
+    //World location
+    public int worldX, worldY;
+
+
+    //images
+    public BufferedImage up1, up2, down1, down2, right1, right2, left1, left2;
+
+    //direction
+    public char direction = 'd';
+
+    //Sprite animation
+    public int spriteCounter = 0;
+    public int spriteNumber = 1;
+
+    //Collision
+    public Rectangle solidArea = new Rectangle(0,0,48,48);
+    public boolean collisionOn = false;
+    public int solidAreaDefaultX, solidAreaDefaultY;
+
+    //Movement
+    public int actionLockCounter = 0;
+
+    //Dialogue
+    String[] dialogue = new String[50];
+    int dialogueIndex = 0;
+
+    //Character Stats
+    public int maxLife;
+    public int life;
+
+    public String name;
+    public int speed;
+    public int level;
+    public int strength;
+    public int dexterity;
+    public int attack;
+    public int defence;
+    public int exp;
+    public int nextLevelExp;
+    public int coin;
+    public int maxMana;
+    public int mana;
+    public Entity currentWeapon;
+    public Entity currentShield;
+
+
+
+
+
+
+
+
+}
